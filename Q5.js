e2b071e6130404c96b25b3e402bd344021d7c961
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:39:48 +0500

    NODEJS Assignment Q5
    5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+// 4.Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Jhon Elia";
+let message = "Qayamat Khaiz Hain Ankhain Tumhari, Akhir tum khowab Kis K Dekhtay Ho";
+console.log(`${famous_person} once said, "${message}"`);
---
+++ Q5.ts
@@ -0,0 +1,6 @@
+// 4.Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person: string = "Jhon Elia";
+let message:String =
+"Qayamat Khaiz Hain Ankhain Tumhari, Akhir tum khowab Kis K Dekhtay Ho"
+console.log(`${famous_person} once said, "${message}"`);
